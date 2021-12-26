391551b5c6ecc06356d61083e092d51bc7b6b1cb
MISHAALO <88333307+MISHAALO@users.noreply.github.com>
2021-12-26 20:55:53 +0300

    Update conf.tf

--- conf.tf
+++ conf.tf
@@ -153,7 +153,7 @@ resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.04.10/24"]
+  v4_cidr_blocks = ["ip"]
 }
 
 #get instance IP
